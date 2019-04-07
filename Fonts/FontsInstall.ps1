b7dff358e6480c0bc3ee746fc659ed224ecf98a0
willclark314 <will314@sohu.com>
2019-04-07 19:11:51 +0800

    edit

--- Fonts/FontsInstall.ps1
+++ Fonts/FontsInstall.ps1
@@ -1,18 +1,32 @@
+# Problem 1
+# when copying files to the cad install path
+# access denied because of the authority
 
-$myFontsPath = 
-	"D:\MyDoc\AutoCAD\Fonts\"
+# Problem 2
+# how to find the install path
+# but not manually
+
+# fonts files must be put in the same path of this script
+# get current path and find the fonts file here
+$source = $PSScriptRoot + "\*.shx"
+
+
+
+
+
+
+#region used for debugging
+$CAD2012FontsPath = 
+	"D:\Desktop\MyFile"
+$CAD2016FontsPath = 
+	"D:\Desktop\MyFile2"
+#endregion
 
 # $CAD2012FontsPath = 
-	# "D:\Desktop\MyFile"
+# 	"C:\Program Files\Autodesk\AutoCAD 2012 - Simplified Chinese\Fonts\"
 # $CAD2016FontsPath = 
-	# "D:\Desktop\MyFile2"
+# 	"C:\Program Files\Autodesk\AutoCAD 2016\Fonts\"
 
-$CAD2012FontsPath = 
-	"C:\Program Files\Autodesk\AutoCAD 2012 - Simplified Chinese\Fonts\"
-$CAD2016FontsPath = 
-	"C:\Program Files\Autodesk\AutoCAD 2016\Fonts\"
-
-$source = $myFontsPath + "*.shx"
 $destination = $CAD2012FontsPath, $CAD2016FontsPath
 
 foreach ($des in $destination)
