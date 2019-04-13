0f08d952d95358d3f8d04e4e1b6937410fb56199
willclark314 <will314@sohu.com>
2019-04-13 21:28:51 +0800

    get to the fonts path via hkml

--- AutoCAD/Fonts/FontsInstall.ps1
+++ AutoCAD/Fonts/FontsInstall.ps1
@@ -1,15 +1,12 @@
 # Problem 1
-# when copying files to the cad install path
-# access denied because of the authority
+# Copy-Item : 对路径“C:\Program Files (x86)\AutoCAD 2008\Fonts\Tssdeng21.shx”的访问被拒绝。
 
-# Problem 2
-# how to find the install path
-# but not manually
+# run this script as
+# Administrator !!!!!!!!!!!!!!!!!!!!!!!!!
+
 function FindInstallPath {
-	param (
-		$var1,
-		$var2
-	)
+	param ()
+
 	# AutoCAD 2012 2016
 	# R18.2 Acad-A001:804
 	# R20.1 Acad-F001:804
@@ -17,6 +14,7 @@ function FindInstallPath {
 	Get-ChildItem HKLM:\SOFTWARE\Autodesk\AutoCAD -recurse -include *Acad-?001:804* |
 	Get-ItemProperty -Name 'acadlocation'|
 	Select-Object -Property acadlocation
+
 	# AutoCAD 2008
 	# Acad-6001:804
 	$myvar2 = # System.Object
@@ -24,33 +22,31 @@ function FindInstallPath {
 	Get-ItemProperty -Name 'acadlocation'|
 	Select-Object -Property acadlocation 
 
-	$myvar1.Get(0).acadlocation
-	# C:\Program Files\Autodesk\AutoCAD 2012 - Simplified Chinese
-	$myvar1.Get(1).acadlocation
-	# C:\Program Files\Autodesk\AutoCAD 2016
-	$myvar2.AcadLocation
-	# C:\Program Files (x86)\AutoCAD 2008
+	# store all acadlocation property in $myvar1
+	$myvar1 += $myvar2
+
+	# translate acadlocation property to string of path
+	# and add the child path \Fonts\
+	$CADFontsPath = @()
+	foreach($var in $myvar1){
+		$CADFontsPath += $var.acadlocation + "\Fonts\"
+	}
+
+	# return string array of path
+	# of all cad installed fonts folder
+	return $CADFontsPath
 }
 
 # fonts files must be put in the same path of this script
 # get current path and find the fonts file here
 $source = $PSScriptRoot + "\*.shx"
+$destination = FindInstallPath
 
-#region used for debugging
-$CAD2012FontsPath = 
-	"D:\Desktop\MyFile"
-$CAD2016FontsPath = 
-	"D:\Desktop\MyFile2"
-#endregion
-
-# $CAD2012FontsPath = 
-# 	"C:\Program Files\Autodesk\AutoCAD 2012 - Simplified Chinese\Fonts\"
-# $CAD2016FontsPath = 
-# 	"C:\Program Files\Autodesk\AutoCAD 2016\Fonts\"
-
-$destination = $CAD2012FontsPath, $CAD2016FontsPath
-
+# copy the fonts(.shx files) to cad 20xx font folder
 foreach ($des in $destination)
 {
-    xcopy $source $des /e
+	# run this script as
+	# Administrator !!!!!!!!!!!!!!!!!!!!!!!!!
+	# Copy-Item : 对路径“C:\Program Files (x86)\AutoCAD 2008\Fonts\Tssdeng21.shx”的访问被拒绝。
+	Copy-Item -Path $source -Destination $des
 }
