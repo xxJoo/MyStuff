b1706b4d4e98e91d5dbd6050bd16b1554e0e082d
dell-PC\dell <will314@sohu.com>
2019-04-15 18:38:46 +0800

    calculator

---
+++ CalcutaionTemp/Calculation.py
@@ -0,0 +1,19 @@
+# coding=utf8
+# CalcutaionTemp/Calcutation.py
+
+
+def Calculation():
+
+    resultDict = {}
+    resultDict['a'] = a = 5
+    resultDict['b'] = b = 2
+    resultDict['c'] = c = a + b
+
+    print c
+
+    return resultDict
+
+if __name__ == '__main__':
+    print __name__
+    Calculation()
+
