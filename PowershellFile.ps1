b052cfdeff9a258b7cf7b1f304763c8869a1be8f
dell-PC\dell <will314@sohu.com>
2019-10-11 09:45:42 +0800

    edit on dell

--- PowershellFile.ps1
+++ PowershellFile.ps1
@@ -26,15 +26,13 @@ False
 True
 # TODO 这句什么意思
 
-
 # 模块
 #############################################################
 # 查看已导入模块
 Get-Module
 # 查看可用模块
 Get-Module -ListAvailable
-# 导入模块
-# 在模块文件夹父目录下
+# 在模块文件夹父目录下 导入模块
 Import-Module WindowsVersion
 # 下载和安装
 $currentPath = Get-Location
