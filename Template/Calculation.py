2d61ff8ab8e443d6e341db0431a692e25a395e48
willclark314 <will314@sohu.com>
2019-11-23 00:29:58 +0800

    edit on mymor

--- Template/Calculation.py
+++ Template/Calculation.py
@@ -1,19 +1,17 @@
 # coding=utf8
 # CalcutaionTemp/Calcutation.py
 
-
+from Template
 def Calculation():
+    a = 1
+    b = 2
+    c = a + b
+    return locals()
 
-    resultDict = {}
-    resultDict['a'] = a = 5
-    resultDict['b'] = b = 2
-    resultDict['c'] = c = a + b
-
-    print (c)
-
-    return resultDict
+    # return resultDict
 
 if __name__ == '__main__':
-    print (__name__)
-    Calculation()
+    
+    dic_value = Calculation()
+    print(dic_value)
 
