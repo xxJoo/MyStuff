d9ecfae66812b96dcf059d04cf4708268af56f7d
willclark314 <will314@sohu.com>
2019-11-24 23:34:08 +0800

    edit on mymor

--- Template/Calculation.py
+++ Template/Calculation.py
@@ -1,32 +1,16 @@
 # coding=utf8
 # CalcutaionTemp/Calcutation.py
 
-import os
-path_of_this_file = os.path.abspath(os.path.dirname(__file__))
 
-from string import Template
-class MyTemplate(Template):
-    delimiter = '#'
+
 def calculation():
     '''计算逻辑'''
     a = 1
     b = 2
     c = a + b
     return locals()
-def useTemplate(dic_var, full_name):
-    with open(full_name, 'r', encoding='utf-8') as tex_file:
-        temp_content = tex_file.read()
-    # 替换模板文本
-    s = MyTemplate(temp_content)
-    res = s.substitute(dic_var)
-    return res
-def calcSheet():
-    # 保存过程变量
-    dic_var = calculation()
-    # 计算书模板路径
-    full_name = os.path.join(path_of_this_file,'CalTemp.tex')
-    # 计算书
-    res = useTemplate(dic_var, full_name)
-    print(res)
+
 if __name__ == '__main__':
-    calcSheet()
+    import os
+    path_of_this_file = os.path.abspath(os.path.dirname(__file__))
+    calculation()
--- Template/main.py
+++ Template/main.py
@@ -1,51 +1,28 @@
 # coding=utf8
+# CalcutaionTemp/Calcutation.py
 
 import os
-import fileinput
-
+# 当前文件路径
+path_of_this_file = os.path.abspath(os.path.dirname(__file__))
+# 计算书模板路径
+path_of_template = os.path.join(path_of_this_file,'CalTemp.tex')
+# 计算过程
+from Calculation import calculation
 from string import Template
-from Calculation import Calculation
-
-
 class MyTemplate(Template):
-    delimiter = "#"
-    idpattern = "[_a-z0-9]*"
+    delimiter = '#'
 
-# Function: ReadFileToString
-# input: [string]fileName (the name of a file)
-# return: [string]text
-def ReadFileToString(fileName):
-    lines = []
-    for line in fileinput.input(fileName):
-        lines.append(line)
-    text = ''.join(lines)
-    return text
-
-# Function: main
-# write the myText to file '__ResultCaltulationSheet.tex'
-def main():
-
-    # get current path of this script
-    pathCurrent = os.path.dirname(__file__) 
-
-    # input: template of current path 'CalTemp.tex'
-    # convert the file to string myText
-    myText = ReadFileToString(pathCurrent + '\\CalTemp.tex')
-    
-    # initiate a template from string myText
-    t = MyTemplate(myText)
-
-    # get the symbols and values from calculation
-    subParams = Calculation()
-    
-    # replace symbols in template with value
-    mySubedText = t.substitute(subParams)
-
-    calculationSheet = open(pathCurrent + '\\__ResultCaltulationSheet.tex', 'w')
-    calculationSheet.write(mySubedText)
-    calculationSheet.close
-
+def useTemplate(dic_var, full_name):
+    with open(full_name, 'r', encoding='utf-8') as tex_file:
+        temp_content = tex_file.read()
+    # 替换模板文本
+    s = MyTemplate(temp_content)
+    res = s.substitute(dic_var)
+    return res
 
 if __name__ == '__main__':
-    main()
-
+    # 保存过程变量
+    dic_var = calculation()
+    # 计算书
+    res = useTemplate(dic_var, path_of_template)
+    print(res)
